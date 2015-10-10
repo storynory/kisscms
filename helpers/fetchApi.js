df29ca5de2d46a9132ad929526ca2d908b9b1f33
hugh Fraser <hugh@storynory.com>
2015-10-10 10:56:09 +0100

    Refactored fetchApi into two functions with a callback
    We now have apiCall() with callback sendRiotTemplate().
    This seperation  should make them easier to combine apiCall with other tempatate types (jade, react, etc).
    It seems to work  but I can see it is not quite right.
    Todos
    Write tests
    Pass in the name of the callBack rather than just hardcode it in.
    Call the functions in the route file rather than the helper file
    Might use async

--- helpers/fetchApi.js
+++ helpers/fetchApi.js
@@ -1,36 +1,35 @@
 module.exports = function fetchApi( req, res, api, template ) {
-  var riot = require('riot')
-  , request = require('superagent')
-  , postView = require(template)
-  , Entities = require('html-entities').XmlEntities
-  , entities = new Entities()
+  var riot = require( 'riot' ),
+    request = require( 'superagent' ),
+    async = require( 'async' ),
+    postView = require( template ),
+    Entities = require( 'html-entities' )
+    .XmlEntities,
+    entities = new Entities()
 
+  var apiCall = function ( req, res, api, callback ) {
+    request.get( api )
+      .end( function ( err, result ) {
+        if ( result && typeof result.body[ 0 ] !== 'undefined' && result.body[ 0 ] !== null ) {
+          sendRiotTemplate( postView, result.body[ 0 ], res )
+        } else {
 
-  request.get( api )
-    .end(function(err, result){
-      if  (result.ok && typeof result.body[0] !== 'undefined' && result.body[0] !== null ) {
+          res.status( 404 ) // HTTP status 404: NotFound
+          .send( '404 Page Not found' );
+        }
 
-    var html = riot.render( postView, { posts: result.body[0]  } );
+      } );
+  };
 
-    html = entities.decode(html)
+  var sendRiotTemplate = function ( postView, result, res ) {
+    var html = riot.render( postView, {
+      posts: result
+    } );
+    html = entities.decode( html )
+    res.send( "<!doctype html>" + html );
 
-    res.send("<!doctype html>" + html );
+  };
 
-  }
+  apiCall( req, res, api, sendRiotTemplate );
 
-  else {
-
-    res.status(404)        // HTTP status 404: NotFound
-   .send('404 Page Not found');
-
-
-    }
-
-
-  });
 };
-
-
-
-
-
