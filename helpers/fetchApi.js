1bc41e7dd71b5eed6fe7ee576b69f6bae7fea0ea
hugh Fraser <hugh@storynory.com>
2015-10-12 12:48:31 +0100

    fetchApi.js is now exported as an object
    The helper / controller fetchApi is now and object with exports.apiCall and exports.sendRiot.  The function is called from postRoute.js.  This gives flexibility to call it in various ways, such as to call a Riot template or a react template.

--- helpers/fetchApi.js
+++ helpers/fetchApi.js
@@ -1,35 +1,31 @@
-module.exports = function fetchApi( req, res, api, template ) {
-  var riot = require( 'riot' ),
-    request = require( 'superagent' ),
-    async = require( 'async' ),
+var exports = module.exports = {};
+
+exports.apiCall = function ( req, res, api, template, callback ) {
+  request = require( 'superagent' ),
+  request.get( api )
+    .end( function ( err, result ) {
+      if ( result && typeof result.body[ 0 ] !== 'undefined' && result.body[ 0 ] !== null ) {
+        callback( template, result.body[ 0 ], res )
+      } else {
+
+        res.status( 404 ) // HTTP status 404: NotFound
+        .send( '404 Page Not found' );
+      }
+
+    } );
+};
+
+exports.sendRiot = function ( template, result, res ) {
+  var riot = require( 'riot' ), // riot has to be called before postview
     postView = require( template ),
     Entities = require( 'html-entities' )
     .XmlEntities,
-    entities = new Entities()
+    entities = new Entities(),
 
-  var apiCall = function ( req, res, api, callback ) {
-    request.get( api )
-      .end( function ( err, result ) {
-        if ( result && typeof result.body[ 0 ] !== 'undefined' && result.body[ 0 ] !== null ) {
-          sendRiotTemplate( postView, result.body[ 0 ], res )
-        } else {
-
-          res.status( 404 ) // HTTP status 404: NotFound
-          .send( '404 Page Not found' );
-        }
-
-      } );
-  };
-
-  var sendRiotTemplate = function ( postView, result, res ) {
-    var html = riot.render( postView, {
+    html = riot.render( postView, {
       posts: result
     } );
-    html = entities.decode( html )
-    res.send( "<!doctype html>" + html );
-
-  };
-
-  apiCall( req, res, api, sendRiotTemplate );
+  html = entities.decode( html )
+  res.send( "<!doctype html>" + html );
 
 };
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,8 +1,8 @@
-exports.post = function ( req, res) {
-  var fetchApi = require("../helpers/fetchApi")
-  , api = "http://localhost:8080/api/posts/?name=" + req.url.slice(1)
-  , template = '../views/templates/page.tag'
+exports.post = function ( req, res ) {
+  var controller = require( "../helpers/fetchApi" ),
+    api = "http://localhost:8080/api/posts/?name=" + req.url.slice( 1 ),
+    template = '../views/templates/page.tag'
 
-  fetchApi(req,res, api , template)
+  controller.apiCall( req, res, api, template, controller.sendRiot )
 
 };
